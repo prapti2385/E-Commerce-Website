51509a65829d25430ea1bb4990c64b6b2457dd27
prapti2385 <praptikhandelwal14@gmail.com>
2025-08-02 23:33:21 +0530

    upto latest collection

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,9 +1,18 @@
 import { createContext } from "react";
-
+import { products } from "../assets/assets.js";
 export const ShopContext = createContext();
 
-const ShopContextProvider = () => {
-    const value = {
-        
-    }
-}
+const ShopContextProvider = ({children}) => {
+  const currency = "₹";
+  const delivery_fee = 10;
+  const value = {
+    products,
+    currency,
+    delivery_fee,
+  };
+  return (
+    <ShopContext.Provider value={value}>{children}</ShopContext.Provider>
+  );
+};
+
+export default ShopContextProvider;
