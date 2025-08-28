abbd401f1be1dfb3266dc79c3412148a1b84c15e
prapti2385 <praptikhandelwal14@gmail.com>
2025-08-29 01:34:35 +0530

    Order Page admin panel and user dashboard

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -29,7 +29,15 @@ const placeOrderStripe = async (req, res) => {};
 const placeOrderRazorpay = async (req, res) => {};
 
 // All orders data for Admin Panel
-const allOrders = async (req, res) => {};
+const allOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 // User Order Data for frontend
 const userOrders = async (req, res) => {
@@ -44,7 +52,16 @@ const userOrders = async (req, res) => {
 };
 
 // update order status from Admin Panel
-const updateStatus = async (req, res) => {};
+const updateStatus = async (req, res) => {
+  try {
+    const { orderId, status } = req.body;
+    await orderModel.findByIdAndUpdate(orderId, { status });
+    res.json({ success: true, message: "Status Updated" });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
 
 export {
   placeOrder,
