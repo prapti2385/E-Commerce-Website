9b73aebc15c6cad3d9ec6d44b5175a3da5f9693a
prapti2385 <praptikhandelwal14@gmail.com>
2025-08-30 03:29:14 +0530

    mistake correction

--- admin/src/components/Login.jsx
+++ admin/src/components/Login.jsx
@@ -48,7 +48,7 @@ const Login = ({ setToken }) => {
             <input
               className="rounded-md w-full px-3 py-2 border brder-gray-300 outline-none"
               type="password"
-              placeholder="Enter your papssword"
+              placeholder="Enter your password"
               required
               onChange={(event) => setPassword(event.target.value)}
               value={password}
