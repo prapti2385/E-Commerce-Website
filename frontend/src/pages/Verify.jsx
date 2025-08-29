627150babdf62915ba217a473dc7681908a48442
prapti2385 <praptikhandelwal14@gmail.com>
2025-08-30 03:53:54 +0530

    deployment

--- frontend/src/pages/Verify.jsx
+++ frontend/src/pages/Verify.jsx
@@ -5,10 +5,9 @@ import { useSearchParams } from "react-router-dom";
 import { useEffect } from "react";
 import { toast } from "react-toastify";
 import axios from "axios";
-import { backendUrl } from "../context/ShopContext";
 
 const Verify = () => {
-  const { navigate, token, setCartItems } = useContext(ShopContext);
+  const { navigate, token, setCartItems, backendUrl } = useContext(ShopContext);
   const [searchParams, setSearchParams] = useSearchParams();
   const success = searchParams.get("success");
   const orderId = searchParams.get("orderId");
